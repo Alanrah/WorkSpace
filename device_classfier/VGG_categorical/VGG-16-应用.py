802e9420cd8219875feb45bad9a8138f33deea3a
Alanrah <18210623224@163.com>
2018-03-07 15:11:45 +0800

    备份

---
+++ device_classfier/VGG_categorical/VGG-16-应用.py
@@ -0,0 +1,58 @@
+import numpy as np
+#keras的图像预处理(single/batch)http://keras-cn.readthedocs.io/en/latest/preprocessing/image/
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D,Activation, Dropout, Flatten, Dense
+from keras import applications
+from keras.models import Model
+from keras.layers import Input
+from keras.preprocessing import image
+
+#  预处理 图像编码服从规定，譬如,RGB，GBR这一类的，preprocess_input(x)
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+'''
+input_shape=(1,28,28)
+img_input = Input(shape=input_shape)
+# Block 1
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+# Block 2
+x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+# Block 3
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+# Block 4
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+# Block 5
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+x = Flatten(name='flatten')(x)
+x = Dense(4096, activation='relu', name='fc1')(x)
+x = Dense(4096, activation='relu', name='fc2')(x)
+x = Dense(classes, activation='softmax', name='predictions')(x)
+'''
+model = applications.VGG16(weights='imagenet')
+img = image.load_img('DELL-R230.jpg', target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+preds = model.predict(x)
+# decode_predictions 输出5个最高概率：(类名, 语义概念, 预测概率) decode_predictions(y_pred)
+for results in decode_predictions(preds):
+    for result in results:
+        print('Probability %0.2f%% => [%s]' % (100*result[2], result[1]))#result是一个元组（class，probability）、、、、
